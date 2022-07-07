bb251e0192b69da60ce353a661a8320b05e6c041
fabio.d.mota <fabio.d.mota@cgi.com>
2022-07-07 12:36:26 +0100

    TRY CONFIGMAP

--- src/components/services/dashboard-api.js
+++ src/components/services/dashboard-api.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_DASHBOARD_URL;
 
 // Actions
 
 export function getAll(){
-    return axios.get(apiUrl).then(res => res.data)
+    return axios.get(process.env.REACT_APP_DASHBOARD_URL).then(res => res.data)
     .catch(err => err)
 }
 
