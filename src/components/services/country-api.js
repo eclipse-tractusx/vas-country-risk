999a50caf39238df676b74e72427d09d87ae47ed
fabio.d.mota <fabio.d.mota@cgi.com>
2022-09-26 18:09:20 +0100

    fix auth

--- src/components/services/country-api.js
+++ src/components/services/country-api.js
@@ -4,19 +4,14 @@ import axios from "axios";
 // Actions
 export function getCountrys(token, customerUser) {
   return axios
-    .get(
-      process.env.REACT_APP_GET_COUNTRYS,
-      {
-        params: {
-          name: customerUser.name,
-          email: customerUser.email,
-          company: customerUser.company,
-        },
+    .get(process.env.REACT_APP_GET_COUNTRYS, {
+      params: {
+        name: customerUser.name,
+        email: customerUser.email,
+        company: customerUser.company,
       },
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    )
+      headers: { Authorization: `Bearer ${token}` },
+    })
     .then((res) => res.data)
     .catch((err) => []);
 }
