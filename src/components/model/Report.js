639ef19c62f6af1bd9d752e5c28be5eac591d536
fabio.d.mota <fabio.d.mota@cgi.com>
2022-10-24 18:28:29 +0100

    add missing module

---
+++ src/components/model/Report.js
@@ -0,0 +1,9 @@
+export class Report {
+  constructor(reportName, companyUserName, company, type, reportValues) {
+    this.reportName = reportName;
+    this.companyUserName = companyUserName;
+    this.company = company;
+    this.type = type;
+    this.reportValues = reportValues;
+  }
+}
