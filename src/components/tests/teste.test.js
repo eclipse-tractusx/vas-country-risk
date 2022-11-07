0f52f617c771446c03445cf52687537eb9a85598
alexsilva <alexandre.b.silva@cgi.com>
2022-11-07 14:42:48 +0000

    test sonarcloud

---
+++ src/components/tests/teste.test.js
@@ -0,0 +1,17 @@
+
+test('test', () => {
+    expect(true).toBe(true);
+})
+
+/* 
+
+import {render, screen, cleanup} from '@testing-library/react'
+import DatePicker from '../dashboard/DatePicker/DatePicker'
+
+test('Render Picker Component', () => {
+    render(<DatePicker/>);
+    const todoElement = screen.getAllByTestId('id1');
+    expect(todoElement).toBeInTheDocument();
+})
+
+*/
