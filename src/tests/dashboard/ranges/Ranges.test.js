a53cad6596ece341e856bc5a668163794a16471d
fabio.d.mota <fabio.d.mota@cgi.com>
2022-11-17 16:16:18 +0000

    more coverage v4

--- src/tests/dashboard/ranges/Ranges.test.js
+++ src/tests/dashboard/ranges/Ranges.test.js
@@ -1,10 +1,14 @@
 import { render, act } from "@testing-library/react";
 import { test } from "@jest/globals";
 import RangeSlider from "../../../components/dashboard/RangeSlider/RangeSlider";
-import { getAllRanges } from "../../../components/services/ranges-api";
+import {
+  getAllRanges,
+  sendValues,
+} from "../../../components/services/ranges-api";
 import "@testing-library/jest-dom/extend-expect";
 import { RangesProvider } from "../../../contexts/ranges";
 import { ReportProvider } from "../../../contexts/reports";
+import userEvent from "@testing-library/user-event";
 
 const range = [
   {
@@ -16,29 +20,31 @@ const range = [
       id: 0,
       name: "John",
       email: "John@email.com",
-      company: "TestCompany"
-    }
+      company: "TestCompany",
+    },
   },
 ];
 
-
 jest.mock("../../../components/services/ranges-api", () => ({
-    getAllRanges: jest.fn(() => range),
+  getAllRanges: jest.fn(() => range),
+  sendValues: jest.fn(),
 }));
 
 test("Ranges Test", async () => {
-    getAllRanges.mockImplementation(() => Promise.resolve(range));
+  getAllRanges.mockImplementation(() => Promise.resolve(range));
+  sendValues.mockImplementation(() => Promise.resolve(range));
   const customerUser = { name: "test" };
   console.log(customerUser);
   let getByText;
   await act(async () => {
     ({ getByText } = render(
-        <ReportProvider>
-            <RangesProvider>
-                <RangeSlider />
-            </RangesProvider>
-        </ReportProvider>
+      <ReportProvider>
+        <RangesProvider>
+          <RangeSlider />
+        </RangesProvider>
+      </ReportProvider>
     ));
   });
   expect(getByText("Save Ranges")).toBeInTheDocument();
+  await userEvent.click(getByText("Save Ranges"));
 });
