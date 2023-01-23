605f38f2b620d6c8c4c4bc284dec9b211abdc3eb
fabio.d.mota <fabio.d.mota@cgi.com>
2023-01-23 21:00:52 +0000

    add detail dialog

---
+++ src/tests/dashboard/detaildialog/DetailDialog.test.js
@@ -0,0 +1,31 @@
+import { render, act } from "@testing-library/react";
+import { test } from "@jest/globals";
+import "@testing-library/jest-dom/extend-expect";
+import DetailDialog from "../../../components/dashboard/DetailDialog/DetailDialog";
+
+const row = [
+  {
+    id: 1,
+    bpn: "BPN-NUMBER",
+    legalName: "Divape Company",
+    address: "15874 Sutteridge Trail",
+    city: "Covilhã",
+    country: "Portugal",
+    score: 10,
+    rating: "Fake Rating",
+    longitude: "107.6185727",
+    latitude: "-6.6889038",
+  },
+];
+
+test("DetailDialog Test", async () => {
+  let getByText;
+  await act(async () => {
+    ({ getByText } = render(
+      <DetailDialog
+        selectedDetailRow={row}
+        onCloseDetailGridFunction={jest.fn()}
+      />
+    ));
+  });
+});
