f103b02fe6dcac40cf05b0d7d5288b9f9066e4af
fabio.d.mota <fabio.d.mota@cgi.com>
2024-05-14 00:50:28 +0100

    feat(frontend): Add new Field on negotiation catalog

--- frontend/src/components/dashboard/NavigationBar/UserInformation/UserInfo.js
+++ frontend/src/components/dashboard/NavigationBar/UserInformation/UserInfo.js
@@ -56,10 +56,12 @@ export const UserInfo = () => {
     },
   ];
 
+  const cleanedRoles = companyUser.roles.map((role) => role.trim());
   // Add negotiation link for users with Negotiator or Admin roles
   if (
-    companyUser.roles.includes("Negotiator") ||
-    companyUser.roles.includes("Admin")
+    cleanedRoles.includes("Negotiator") ||
+    cleanedRoles.includes("Admin") ||
+    cleanedRoles.includes("Company Admin")
   ) {
     menuItems = [
       {
@@ -87,5 +89,3 @@ export const UserInfo = () => {
     </div>
   );
 };
-
-export default UserInfo;
